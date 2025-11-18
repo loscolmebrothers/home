66a9b2ad4493417ea7312dfd392e9373b6d9078d
Daniel Colmenares Salamanca <hello@danicolms.xyz>
2025-11-18 20:22:36 +0100

    fix base path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/home/',
   plugins: [react()],
   test: {
     globals: true,
